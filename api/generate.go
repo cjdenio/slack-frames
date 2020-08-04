cce9847f2efba78ef22533d5602cec6dbd43fb6a
Caleb Denio <cjdenio44@gmail.com>
2020-08-04 17:00:20 -0400

    Dead code :(

--- api/generate.go
+++ api/generate.go
@@ -1,17 +1,22 @@
 package api
 
 import (
+	"fmt"
 	"image"
 	"image/draw"
 	"image/jpeg"
 	"image/png"
+	"io"
+	"io/ioutil"
 	"log"
+	"mime/multipart"
 	"os"
+
+	"github.com/nfnt/resize"
 
 	"net/http"
 
 	"github.com/gorilla/sessions"
-	"github.com/nfnt/resize"
 	"github.com/slack-go/slack"
 )
 
@@ -33,14 +38,33 @@ func Generate(res http.ResponseWriter, req *http.Request) {
 		log.Fatal(err)
 		return
 	}
-	res.Write([]byte(userProfile.ImageOriginal))
-	return
 
-	profile, _ := os.Open("profile.JPG")
-	frame, _ := os.Open("frame.png")
+	avatar, err := http.Get(userProfile.ImageOriginal)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 
-	profileImg, _ := jpeg.Decode(profile)
-	frameImg, _ := png.Decode(frame)
+	frame, err := http.Get(req.URL.Query().Get("frame"))
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	fmt.Println(userProfile.ImageOriginal)
+
+	res.Header().Add("Content-type", "image/png")
+
+	//io.Copy(res, frame.Body)
+
+	profileImg, err := jpeg.Decode(avatar.Body)
+	if err != nil {
+		fmt.Println("profileerr " + err.Error())
+	}
+	frameImg, err := png.Decode(frame.Body)
+	if err != nil {
+		fmt.Println("frameerr " + err.Error())
+	}
 	frameImg = resize.Resize(uint(profileImg.Bounds().Dx()), uint(profileImg.Bounds().Dy()), frameImg, resize.Bilinear)
 
 	finalImg := image.NewRGBA(profileImg.Bounds())
@@ -48,7 +72,34 @@ func Generate(res http.ResponseWriter, req *http.Request) {
 	draw.Draw(finalImg, profileImg.Bounds(), profileImg, image.Point{}, draw.Over)
 	draw.Draw(finalImg, profileImg.Bounds(), frameImg, image.Point{}, draw.Over)
 
-	final, _ := os.Create("final.png")
+	res.Header().Add("Content-type", "image/png")
 
-	png.Encode(final, resize.Resize(512, 512, finalImg, resize.Bilinear))
+	r, w := io.Pipe()
+	body := multipart.NewWriter(w)
+	textW, _ := body.CreateFormField("moo")
+
+	textW.Write([]byte("moo"))
+
+	//imageW, _ := body.CreateFormFile("image", "image")
+
+	//err = png.Encode(imageW, finalImg)
+	err = body.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+	w.Close()
+
+	read, _ := ioutil.ReadAll(r)
+	fmt.Println(string(read))
+
+	// resp, err := http.Post("https://slack.com/api/users.setPhoto", "multipart/form-data", r)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	// stuff, _ := ioutil.ReadAll(resp.Body)
+
+	// fmt.Println(string(stuff))
+
+	res.Write(nil)
 }
